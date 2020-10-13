5c7dfcffeaccc42ecc33a385f8ac734697f86c4a
R K Sharma <sonal9108@gmail.com>
2020-10-14 01:29:22 +0530

    Repo

---
+++ src/main/java/com/example/PromotionEngine/PromotionEngine/Repository/ItemRepository.java
@@ -0,0 +1,8 @@
+package com.example.PromotionEngine.PromotionEngine.Repository;
+
+import com.example.PromotionEngine.PromotionEngine.Entity.ItemAll;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<ItemAll,Integer> {
+
+}
