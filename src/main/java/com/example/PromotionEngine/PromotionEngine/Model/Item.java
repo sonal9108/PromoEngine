8c67fc6c07f03f8e6468f6727b2a93226f78e3bd
R K Sharma <sonal9108@gmail.com>
2020-10-14 01:31:51 +0530

    item

---
+++ src/main/java/com/example/PromotionEngine/PromotionEngine/Model/Item.java
@@ -0,0 +1,23 @@
+package com.example.PromotionEngine.PromotionEngine.Model;
+
+public class Item {
+
+    private String iName;
+    private Integer iQuantity;
+
+    public String getiName() {
+        return iName;
+    }
+
+    public void setiName(String iName) {
+        this.iName = iName;
+    }
+
+    public Integer getiQuantity() {
+        return iQuantity;
+    }
+
+    public void setiQuantity(Integer iQuantity) {
+        this.iQuantity = iQuantity;
+    }
+}
